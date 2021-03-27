490dfad7076e675e567600f3a8ad3c5d55bd01a0
Shai <shai4life@gmail.com>
2021-03-26 20:25:20 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String firstName = "Shai";
+			char middleName = 'M';
+			String lastName = "Alnwick";
+			String fullName = firstName + " " + middleName + " " + lastName;
+			System.out.println(fullName);
+			
+			double cashInwallet = 100.05;
+			int item = 5;
+			double amountInWalletAfterPurchase = cashInwallet - item;
+			System.out.println(amountInWalletAfterPurchase);
+			
+			
+			
+			
+	}
+
+}
